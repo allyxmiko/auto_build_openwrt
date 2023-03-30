e9126a120ca4b28fb01deb0340d8a354f2dd9324
Allyx <53589496+allyxmiko@users.noreply.github.com>
2023-03-30 09:03:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,11 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# echo 'src-git packages https://github.com/coolsnowwolf/packages' >> feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
-echo 'src-git routing https://github.com/coolsnowwolf/routing' >> feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >> feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-# echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >> feeds.conf.default
+echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >> feeds.conf.default
 echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
